cbe447f7754e7e4edd87b993d74901642d423fab
mariem-werghi <mariemwerghi20@gmail.com>
2023-07-20 12:43:23 +0100

    cp-html

---
+++ index.js
@@ -0,0 +1,11 @@
+// function f (){
+
+//     console.log("hello");
+
+
+// }
+// var fo =f()
+
+var test =document.getElementById("butn")
+
+test.addEventListener("click",function(){console.log("hello")})
